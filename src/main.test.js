0d426ba7207d2624d5885853301234ca30d39706
marcelo <celocarvalho7@gmail.com>
2023-06-28 17:40:04 -0300

    Commit de um bug

--- src/main.test.js
+++ src/main.test.js
@@ -4,6 +4,6 @@ const sum = require('./main')
 describe('sum()', () => {
     test('Given 2 and 3, should sums 5', () => {
     
-      expect(sum(2)(3)).toEqual(5)
+      expect(sum(2)(3)).toEqual(6)
     })
   })
